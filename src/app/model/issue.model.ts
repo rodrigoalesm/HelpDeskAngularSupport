e1369ae8c0c62e2ed898f59fcbdcc93bb6cc0554
rodrigo <rodrigoalesm@gmail.com>
2020-02-16 14:57:37 -0600

    IssueModelService

---
+++ src/app/model/issue.model.ts
@@ -0,0 +1,18 @@
+export class Issue {
+    id: number;
+    idClient: number;
+    description: string;
+    timeStamp: Date;
+    contactPhone: string;
+    contactEmail: string;
+    classification: string;
+    status: string;
+    serviceType: string;
+    name: string;
+    secondName: string;
+    firstName: string;
+    address: string;
+    phone: string;
+    secondContact: string;
+    email: string;
+}
---
+++ src/app/services/issue.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Issue } from '../model/issue.model'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IssueService {
+
+  private baseUrl = 'http://localhost:8080/issues/';
+
+  constructor(private http: HttpClient) { }
+
+  getIssue(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}${id}`);
+  }
+
+  createIssue(issue: Issue): Observable<any> {
+    return this.http.post(`${this.baseUrl}`, issue);
+  }
+
+  updateIssue(id: number, issue: Issue): Observable<any> {
+    return this.http.put(`${this.baseUrl}${id}`, issue);
+  }
+
+  deleteIssue(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}${id}`, { responseType: 'text' });
+  }
+
+  getIssuesList(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+}
