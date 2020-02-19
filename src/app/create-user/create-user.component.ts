56b4cac421125d8001f9c60e1865aab61faf0d08
gloryaj <37586079+gloryaj@users.noreply.github.com>
2020-02-19 14:07:26 -0600

    user-create.ts

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,16 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
-Component({
+@Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
   styleUrls: ['./create-user.component.css']
 })
-export class CreateUserComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class CreateUserComponent{
+ 
+  userForm: FormGroup;
+  title: string= "Create";
+  id:number;
+  errorMessage: any;
+  constructor(private _fb: FormBuilder, private _avRoute: ActivatedRoute,
+    private _userService: UserService, private _router: Router) { 
+    if (this._avRoute.snapshot.params["id"]) {
+      this.id = this._avRoute.snapshot.params["id"];
+    }
+    this.userForm= this._fb.group({
+      name: ['', [Validators.required]],
+      secondLastName: ['', [Validators.required]],
+      firstLastName: ['', [Validators.required]],
+      address: ['', [Validators.required]],
+      phone: ['', [Validators.required]],
+      secondContact: ['', [Validators.required]],
+      userRole: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    })
   }
-
+   
+    save(){
+      if (!this.userForm.valid) {
+        return;
+      }
+      if (this.title == "Create") {
+        this._userService.saveUser(this.userForm.value)
+          .subscribe((data) => {
+            //this._router.navigate(['/fetch-employee']);
+            
+          }, error => this.errorMessage = error)
+      } 
+    }
+  
 
 }
