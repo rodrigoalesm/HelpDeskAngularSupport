6c1b6855798f52b348e07c675a4e3f9552975953
gloryaj <37586079+gloryaj@users.noreply.github.com>
2020-02-26 23:50:18 -0600

    Update create-user.ts

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,49 +1,46 @@
 import { UserService } from '../services/user.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
   styleUrls: ['./create-user.component.css']
 })
-export class CreateUserComponent{
- 
+export class CreateUserComponent implements OnInit {
   userForm: FormGroup;
-  title: string= "Create";
-  id:number;
-  errorMessage: any;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  title: string = "Create";
+  id: number;
+  error: '';
   constructor(private _fb: FormBuilder, private _avRoute: ActivatedRoute,
-    private _userService: UserService, private _router: Router) { 
-    if (this._avRoute.snapshot.params["id"]) {
-      this.id = this._avRoute.snapshot.params["id"];
-    }
-    this.userForm= this._fb.group({
-      name: ['', [Validators.required]],
-      secondLastName: ['', [Validators.required]],
-      firstLastName: ['', [Validators.required]],
+    private _userService: UserService, private _router: Router) { }
+
+  ngOnInit() {
+    this.userForm = this._fb.group({
+      username: ['', [Validators.required]],
+      secondname: ['', [Validators.required]],
+      firstname: ['', [Validators.required]],
       address: ['', [Validators.required]],
       phone: ['', [Validators.required]],
-      secondContact: ['', [Validators.required]],
-      userRole: ['', [Validators.required]],
+      secondcontact: ['', [Validators.required]],
       email: ['', [Validators.required]],
       password: ['', [Validators.required]]
-    })
-  }
-   
-    save(){
-      if (!this.userForm.valid) {
-        return;
-      }
-      if (this.title == "Create") {
-        this._userService.saveUser(this.userForm.value)
-          .subscribe((data) => {
-            //this._router.navigate(['/fetch-employee']);
-            
-          }, error => this.errorMessage = error)
-      } 
-    }
-  
+    });
 
+  }
+
+  onSubmit() {
+    if (!this.userForm.valid) {
+      return;
+    }
+    this._userService.saveUser(this.userForm.value)
+      .subscribe((data) => {
+        this._router.navigate(['/fetch-employee']);
+      }, error => this.error = error)
+  }
 }
